ae71117085ff6f7503341145388578ad51ac2d40
Piotr <piotrek@debian>
2016-09-15 14:25:12 +0200

    INIT wyslijEmail.py - wysylacz emaili

---
+++ wyslijEmail.py
@@ -0,0 +1,24 @@
+import smtplib
+import os.path
+import wyslijEmailParams #plik z parametrami konta pocztowego
+
+from email.mime.text import MIMEText
+
+
+def wyslijEmail(do, temat, tresc):
+  msg = MIMEText(tresc)
+  msg['Subject'] = temat
+  msg['From'] = wyslijEmailParams.od
+  s = smtplib.SMTP_SSL(wyslijEmailParams.serwersmtp)
+  s.login(wyslijEmailParams.login,wyslijEmailParams.haslo)
+  s.sendmail(wyslijEmailParams.od, do, msg.as_string())
+  print(' ',do)
+  s.quit
+
+
+
+
+##przykladUzycia
+#wyslijEmail('stefan.potylica@poczta.fm','test','tresc')
+
+
