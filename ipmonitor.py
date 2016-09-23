ea676700f5b021e02ef6ecfa5280dd66cc1346d7
Piotr Oslowski <piotr.oslowski@sedkomp.com.pl>
2016-09-24 00:12:46 +0200

    zmiana hostow na klasy maszyna, poczatki do watkow (dodane importy)

--- ipmonitor.py
+++ ipmonitor.py
@@ -6,6 +6,8 @@ import ipmonitorParams
 import datetime
 import curses
 from curses import wrapper
+import threading
+from queue import Queue
 
 
 stdscr = curses.initscr()
@@ -18,6 +20,19 @@ hosty = csv.reader(open('listaHostow.csv', newline=''), delimiter=';', quotechar
 
 
 hosty2 = list(hosty)
+lock = threading.Lock()
+
+class maszyna:
+  adres = ''
+  nazwa = ''
+  wiersz = 0
+  stan = -1
+  #def __maszyna__(self):
+  #  self.adres =''
+  #  self.nazwa=''
+  #  self.wiersz=0
+  #  self.stan=-1
+
 
 
 def kbfunc():
@@ -46,13 +61,24 @@ def sprawdzacz():
     #while True:
     #w pierwszej linii jest nagloweK
   stany = {}
+  maszyny = []
+  wiersz = 0
+#napelniamy liste maszyny obiektami typu maszyna
   for rowek in hosty2:
     if rowek[0]!='adres':
-      stany[rowek[0]]=-1
-  while True:
-    wiersz = 0
-    for rowek in hosty2:
       wiersz += 1
+      maszynaTmp = maszyna()
+      maszynaTmp.wiersz = wiersz
+      maszynaTmp.nazwa = rowek[1]
+      maszynaTmp.adres = rowek[0]
+      maszynaTmp.stan = -1
+      maszyny.append(maszynaTmp)
+      #stany[rowek[0]]=-1
+  while True:
+    #wiersz = 0
+    #for rowek in hosty2:
+    for m in maszyny:
+      #wiersz += 1
       x = kbfunc()
       if x == 'q':
         curses.nocbreak()
@@ -60,38 +86,40 @@ def sprawdzacz():
         curses.echo()
         curses.endwin()
         exit()
-      if rowek[0]!='adres':
+      #if rowek[0]!='adres':
+      if 1==1:
         #print(rowek)
-        stdscr.addstr(wiersz,0,rowek[0])
-        stdscr.addstr(wiersz,16,rowek[1]+'                       ')
-        stdscr.addstr(wiersz,30,' ---- sprawdzam ----')
+        stdscr.addstr(m.wiersz,0,m.adres)
+        stdscr.addstr(m.wiersz,16,m.nazwa+'                       ')
+        stdscr.addstr(m.wiersz,30,' ---- sprawdzam ----')
         stdscr.refresh()
-        resp = ping(rowek[0],1)#os.system('sudo ping -c 1 '+rowek[0]+' > /dev/null')
-        prev=stany[rowek[0]]
+        resp = ping(m.adres,1)#os.system('sudo ping -c 1 '+rowek[0]+' > /dev/null')
+        prev=m.stan
         #print('prev={}'.format(prev))
         stan = ''
         if resp == 1:
           #print(rowek[0], ' UP')
-          stdscr.addstr(wiersz,30,' UP                  ')
+          stdscr.addstr(m.wiersz,30,' UP                  ')
           stdscr.refresh()
-          stany[rowek[0]]= 1
+          m.stan = 1
           stan='UP'
         else:
-          if ping(rowek[0],4)==0:
+          if ping(m.adres,4)==0:
 #            print(rowek[0], ' DOWN')
-            stdscr.addstr(wiersz ,30, ' DOWN               ')
+            stdscr.addstr(m.wiersz ,30, ' DOWN               ')
             stdscr.refresh()
-            stany[rowek[0]] =0
+            m.stan =0
             stan='DOWN'
         stdscr.refresh()
         #sprawdzamy czy nastapila zmiana statusu
         #print('prev={} stany[rowek[0]]={} rowek[0]={}'.format(prev, stany[rowek[0]], rowek[0]))
-        if (prev!=-1) & (prev != stany[rowek[0]]):
+        if (prev!=-1) & (prev != m.stan):
           try:
-            wyslijEmail.wyslijEmail(ipmonitorParams.do, datetime.datetime.now().strftime('%y-%m-%d %H-%M-%S')+ ' '+stan + ' '+rowek[1]+' '+rowek[0], '')
+            wynik = wyslijEmail.wyslijEmail(ipmonitorParams.do, datetime.datetime.now().strftime('%y-%m-%d %H-%M-%S')+ ' '+stan + ' '+m.nazwa+' '+m.adres, '')
+            stdscr.addstr(0,0, datetime.datetime.now().strftime('%H-%M-%S')+' '+wynik)
           except:
-            print("Blad podczas wysylki email")
-            stdscr.addstr(0,0,datetime.datetime.now().strftime('%y-%m-%d %H-%M-%S')+'Blad podczas wysylki email              ')
+            #print("Blad podczas wysylki email")
+            stdscr.addstr(0,0,datetime.datetime.now().strftime('%H-%M-%S')+' Blad podczas wysylki email              ')
             stdscr.refresh()
 
     time.sleep(5)
