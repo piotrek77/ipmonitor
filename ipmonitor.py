9267c9fdd249d1735c0d3acba98ea94d8457c6e9
Piotr Oslowski <piotr.oslowski@sedkomp.com.pl>
2016-09-23 14:12:46 +0200

    drobne poprawki do curses, usuniete dziwne komentarze

--- ipmonitor.py
+++ ipmonitor.py
@@ -1,7 +1,5 @@
 import os
 import time
-#import turtle
-#from graphics import *
 import csv
 import wyslijEmail
 import ipmonitorParams
@@ -9,16 +7,11 @@ import datetime
 import curses
 from curses import wrapper
 
-#import msvcrt
 
 stdscr = curses.initscr()
 curses.noecho()
 curses.cbreak()
 stdscr.keypad(True)
-
-#stdscr.addstr(0,0,'test')
-#stdscr.refresh()
-
 
 hosty = csv.reader(open('listaHostow.csv', newline=''), delimiter=';', quotechar='|')
 
@@ -28,13 +21,13 @@ hosty2 = list(hosty)
 
 
 def kbfunc():
-  #x = stdscr.getch()
+  
   stdscr.nodelay(True)
   try:
     x = stdscr.getkey()
   except:
     x = ''
-  #x = 'r'
+  
   return x
 
 
@@ -66,7 +59,7 @@ def sprawdzacz():
         stdscr.keypad(False)
         curses.echo()
         curses.endwin()
-        break
+        exit()
       if rowek[0]!='adres':
         #print(rowek)
         stdscr.addstr(wiersz,0,rowek[0])
@@ -104,56 +97,12 @@ def sprawdzacz():
     time.sleep(5)
   
      
-#print(ping('wp.pl',4))
-#def main(stdscr):
-  #main()
-  #sprawdzacz()
 
-#def main():
-sprawdzacz()
+def main(stdscr):
+  
+  sprawdzacz()
 
 
-#wrapper(main)
-#main()
-
-#adres1 = '192.168.1.1'
-#adres11 = '192.168.1.11'
-#zolw = turtle.Turtle()
-#ekran = zolw.screen
-#zolw.write('test')
-#ekran.clear()
 
 
-#win = GraphWin(width = 200, height = 200)
-#win.setCoords(0,100,100,0)
-
-#label1 = Text(Point(20,10), adres1)
-#label1.draw(win)
-
-#label11 = Text(Point(20,30), adres11)
-#label11.draw(win)
-
-#kontrolka1 = Circle(Point(45,9),3)
-#kontrolka1.setOutline('black')
-#kontrolka1.draw(win)
-#kontrolka11 = Circle(Point(45,29),3)
-#kontrolka11.setOutline('black')
-#kontrolka11.draw(win)
-
-#def sprawdzaczGraficzny():
-# while True:
-#  resp1 = os.system("sudo ping -c 1 " + adres1)
-#  resp11 = os.system("sudo ping -c 1 " + adres11)
-#  if resp1==0:
-#        print( adres1, ' UP ')
-        #kontrolka1.setFill('green')
-#  else:
-#        print (adres1, ' DOWN')
-        #kontrolka1.setFill('red')
-#  if resp11==0:
-#        print( adres1, ' UP ')
-        #kontrolka11.setFill('green')
-#  else:
-#        print (adres1, ' DOWN')
-        #kontrolka11.setFill('red')
-# time.sleep(1)
+wrapper(main)
